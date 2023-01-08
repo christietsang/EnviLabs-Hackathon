9564400af5f8c6dc49b48c72cdcb96061871172f
Christie Tsang <ctsang195@gmail.com>
2023-01-08 10:06:54 -0800

    limit drop down size

--- react-frontend/src/sections/@dashboard/app/AppFuelRateByType.js
+++ react-frontend/src/sections/@dashboard/app/AppFuelRateByType.js
@@ -3,47 +3,42 @@ import React, { useState, useEffect } from 'react'
 import { VictoryBar, VictoryChart, VictoryTheme, VictoryGroup } from 'victory';
 
 
-const haulingFuelRates = [
-  { x: 1, y: 2.3 },
-  { x: 2, y: 2.3 },
-  { x: 3, y: 2.3 },
-  { x: 4, y: 2.3 },
-  { x: 5, y: 2.3 },
-]
+const haulingFuelRates = {"data":{"hauling":[{"fuel_rate":205.0365525808979,"truck_type":"0"},{"fuel_rate":205.08348484249228,"truck_type":"1"},{"fuel_rate":203.92440101721795,"truck_type":"3"}],"non_hauling":[{"fuel_rate":197.91279453606396,"truck_type":"0"},{"fuel_rate":197.83919995944515,"truck_type":"1"},{"fuel_rate":197.47581229210033,"truck_type":"3"}]}}
 
 export default function AppFuelRateByType() {
-  
+
   return (
     <div>
       <VictoryChart
         theme={VictoryTheme.material}
-        domain={{ y: [0.5, 5.5] }}
+        domain={{ y: [190, 206] }}
       >
-          <VictoryGroup horizontal
-            offset={10}
-            style={{ data: { width: 6 } }}
-            colorScale={"qualitative"}
-          >
-            <VictoryBar
-              data={[
-                { x: 1, y: 1 },
-                { x: 2, y: 2 },
-                { x: 3, y: 3 },
-                { x: 4, y: 2 },
-                { x: 5, y: 1 }
-              ]}
-            />
-            <VictoryBar
-              data={[
-                { x: 1, y: 2 },
-                { x: 1, y: 3 },
-                { x: 3, y: 4 },
-                { x: 4, y: 5 },
-                { x: 5, y: 5 }
-              ]}
-            />
+        <VictoryGroup horizontal
+          offset={10}
+          style={{ data: { width: 6 } }}
+          colorScale={"qualitative"}
+        >
+          <VictoryBar
+            data={[
+              { y: haulingFuelRates.data.hauling[0].fuel_rate, x: haulingFuelRates.data.hauling[0].truck_type },
+              { y: haulingFuelRates.data.hauling[1].fuel_rate, x: haulingFuelRates.data.hauling[1].truck_type },
+              { y: haulingFuelRates.data.hauling[2].fuel_rate, x: haulingFuelRates.data.hauling[2].truck_type },
+            ]}
+          />
+          <VictoryBar
+            data={[
+              { y: haulingFuelRates.data.non_hauling[0].fuel_rate, x: haulingFuelRates.data.non_hauling[0].truck_type },
+              { y: haulingFuelRates.data.non_hauling[1].fuel_rate, x: haulingFuelRates.data.non_hauling[1].truck_type },
+              { y: haulingFuelRates.data.non_hauling[2].fuel_rate, x: haulingFuelRates.data.non_hauling[2].truck_type },
+            ]}
+          />
         </VictoryGroup>
       </VictoryChart>
     </div>
   );
 }
+
+
+
+
+
